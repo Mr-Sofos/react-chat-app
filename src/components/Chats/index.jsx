91ff6c5a3018f07fed3e31dd66b056fe7ca06d98
Mr-Sofos <mokhmad.tazbiev@gmail.com>
2021-04-27 20:02:09 +0300

    added components chats, messages, MyProfile'

---
+++ src/components/Chats/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Chats (props) {
+  return (
+    <div></div>
+  );
+}
+
+export default Chats;
---
+++ src/components/Messages/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Messages (props) {
+  return (
+    <div></div>
+  );
+}
+
+export default Messages;
