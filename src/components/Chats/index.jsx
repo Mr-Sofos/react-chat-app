0221740729b276f0c27438681037ecf484151b1c
ismail <ismtim095@gmail.com>
2021-04-28 16:30:07 +0300

    Закончил макет для блока с контактами

--- src/components/Chats/index.jsx
+++ src/components/Chats/index.jsx
@@ -1,7 +1,16 @@
 import style from './style.module.css';
+import { AiOutlineSearch } from 'react-icons/all';
 
 function Chats() {
-  return <div className={style.chats}></div>;
+  return <div className={style.chats}>
+    <div className={style.search}>
+      <div className="icon"><AiOutlineSearch /></div>
+      <input type='text' placeholder='Search contact'/>
+    </div>
+    <div className={style.contact}>
+    </div>
+
+  </div>;
 }
 
 export default Chats;
