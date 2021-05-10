ed13751c64549483b763e6a2c3ac73a2105adbf4
Mr-Sofos <mokhmad.tazbiev@gmail.com>
2021-05-10 15:30:08 +0300

    реализовал прокрутку чата до последнего сообщения

---
+++ src/components/utils/utils.js
@@ -0,0 +1,6 @@
+export const scrollChatDown = () => {
+  const chatWindow = document.getElementById("chat-window");
+  if (chatWindow) {
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+  }
+};
